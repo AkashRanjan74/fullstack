7c9cffadd6b5fb941438b4eb071b27298ae68df9
AKki <AKki@gmail.com>
2019-10-02 16:10:39 +0530

    JScript commit

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-alert 'test';
+
+var str="akash ";
+var str1='ranjan ';
+var str2=`"satpathy"
+<h1>hello</h1>
+<p>qwertyuiopasdfghjklzxcvbnm</p>
+<button>Submit</button>
+<input type ="text" placeholder="enter text"/>
+${2+2}
+`; //Template String(es6)
+console.log( str + str1 + str2);
+document.write(str+str1+str2);
+
+var name="akash";
+var age=25;
+var company="infosys";
+var salary=15000;
+console.log("my name is "+name+" my age is "+age+" i work at "+company+" and my salary is "+salary);
+console.log(`my name is ${name} 
+my age is ${age}
+i work at ${company}
+i am getting ${salary}
+`);
+document.write(`my name is  <strong>${name} </strong>
+my age is ${age}
+i work at ${company}
+i am getting ${salary}
+`);
+
