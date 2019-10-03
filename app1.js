44491b50a1637cce3b853399b6db450c955db05b
AKki <AKki@gmail.com>
2019-10-03 16:20:13 +0530

    jscript commands

--- app1.js
+++ app1.js
@@ -7,8 +7,51 @@ Date
 RegExp
 JSON
 
-
-let languages=['java','nodejs','jscript',undefined,null,true];
+//
+let languages=['java','nodejs','jscript',undefined,null,true,'reactjs'];
 console.log(languages);
-console.log(languages[6]);
+console.log(languages[3]);
 console.log(languages.length);
+languages.pop();
+console.log(languages);
+
+//
+var array1 =new Array('nodejs','angularjs','reactjs','python','expressjs');
+console.log(array1);
+
+//After ECMAScript 6
+var array3= Array.of('java','python','c++','c#','.net');
+console.log(array3);
+var array4=Array(7);
+console.log(array4);
+
+//check types of array
+var array5= 'ranjan';
+console.log(typeof array1, typeof array4, typeof array3)
+console.log(Array.isArray(array4));
+console.log(Array.isArray(array1));
+console.log(Array.isArray(array3));
+console.log(Array.isArray(array5));
+console.log(Array.isArray({foo:123}));
+
+//String to Array
+var str="Hello World!";
+var strArray1=str.split("");//es5
+var strArray2=str.split(" ");
+var strArray3=str.split("  ");
+console.log(strArray1);
+console.log(strArray2);
+console.log(strArray3);
+
+var es6way=Array.from(str);//es6
+console.log(es6way);
+
+let[...akash]=str;//array destructuring//spread operator
+console.log(akash.toString());
+console.log(akash.join('-'));
+
+
+//
+function test(){
+
+}
