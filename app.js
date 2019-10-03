e96f1da9341d05969be159892c753ec07d1d240c
AKki <AKki@gmail.com>
2019-10-03 14:53:40 +0530

    jscript datatypes

--- app.js
+++ app.js
@@ -35,3 +35,66 @@ console.log(typeof name);
 console.log(typeof name1);
 console.log(typeof num2);
 console.log(typeof num3);
+
+
+var test1=true;
+var test2=false;
+
+
+undefined
+null
+
+
+var test ;
+var bool=true;
+console.log(typeof test);//Object
+console.log(typeof bool);
+
+var name4='akash';
+name4=10;
+name4=true;
+name4=undefined;
+name4=null;
+
+// let name5='ranjan';
+// let name5=10;
+// let name5=true;
+// let name5=undefined;
+// let name5=null;
+// console.log( name5);
+console.log( name4);
+
+
+// const skills="nodejs";
+// skills=10;
+
+
+// for(var i=1;i<10;i++)
+// {
+//     var name6='sachin';
+//     console.log(i);
+//     //global scope
+// }
+// console.log(name6); 
+
+// for(let i=1;i<10;i++)
+// {
+//     let name7='sachin';
+//     console.log(i);
+//     //blocked scope
+// }
+// console.log(name7); 
+
+// {
+// username="akash";
+// var username;
+// console.log(username);
+// //hoisting working with var
+// }
+// {
+//     username="akash";
+//     let username;
+//     console.log(username);
+//     //hoisting not working with let
+// }
+:😆:
---
+++ app1.js
@@ -0,0 +1,14 @@
+//non primitive datatype or Object reference...🤖
+Array
+Object
+Function
+Math
+Date
+RegExp
+JSON
+
+
+let languages=['java','nodejs','jscript',undefined,null,true];
+console.log(languages);
+console.log(languages[6]);
+console.log(languages.length);
